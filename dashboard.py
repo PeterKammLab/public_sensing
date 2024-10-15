b729b9082a868baa51e03e03b8a0ebd38273e963
Petar Koljensic <104145970+PeterKammLab@users.noreply.github.com>
2024-10-15 13:02:57 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -82,9 +82,9 @@ with tab2:
     # Add the text with specified bold formatting
      # Display initial map
     
-    st.markdown("Based on real-time GTFS data, **GVB trams**, **buses**, and **13th March 2024** in Amsterdam.")
+    st.markdown("based on real-time GTFS data, **GVB trams** * **buses** for **13th March 2024** in Amsterdam")
 
-    st.subheader("")
+    #st.subheader("")
     
     # Define the fixed file path
     file_path = "freq_cbs_1304_fullday.shp"
@@ -157,9 +157,9 @@ with tab2:
 # Assuming you have your Streamlit tab setup before this
 with tab3:
 
-    st.markdown("Based on real-time GTFS data, **GVB trams**, **buses**, and **13th March 2024** in Amsterdam.")
+    st.markdown("based on real-time GTFS data, **GVB trams** * **buses** for **13th March 2024** in Amsterdam")
 
-    st.subheader("")
+    #st.subheader("")
     # Define the mapping of user-friendly names to actual column names for index analysis
     index_label_mapping = {
         'Inhabitants': 'inhab_index',
