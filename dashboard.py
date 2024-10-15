57ffe27219b47b8033c9222961cd0d27691f90dc
Petar Koljensic <104145970+PeterKammLab@users.noreply.github.com>
2024-10-15 12:55:09 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -24,7 +24,7 @@ cbs_filepath = 'cbs_amsterdam_2021_clean.shp'
 st.title("Public Transport Sensing")
 
 # Use tabs instead of sidebar
-tab1, tab2, tab3 = st.tabs(["**Analysis**", "**Frequencies**", "**Weights & Index**"])
+tab1, tab2, tab3 = st.tabs(["**Spatial Analysis**", "**Frequencies**", "**Weights & Index**"])
 
 with tab1:
     # User input for transport type
